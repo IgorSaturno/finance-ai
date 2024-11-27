23634716a7de765abc34e67f875e81c86ced14d1
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2024-11-27 20:33:19 -0300

    feat: ensure only premium users can generate ai report

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import { isMatch } from "date-fns";
@@ -29,6 +29,7 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
   }
   const dashboard = await getDashboard(month);
   const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <Navbar />
@@ -36,7 +37,10 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
-            <AiReportButton month={month} />
+            <AiReportButton
+              month={month}
+              hasPremiumPlan={user.publicMetadata.subscriptionPlan == "premium"}
+            />
             <TimeSelect />
           </div>
         </div>
